ae7d786f47fe61f8c700dfee56a6c3da5236ffb8
Danil Korotenko <danil.korotenko@gmail.com>
2022-10-28 16:01:31 +0300

    Service app target was added.

---
+++ BonjourServiceApp/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  BonjourServiceApp
+//
+//  Created by Danil Korotenko on 10/28/22.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
